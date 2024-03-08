c2bfc5530c395b296e0ff949551b3c4ea87d4ce9
seslly <161339320+seslly@users.noreply.github.com>
2024-03-08 14:59:26 -0800

    feat: echo command trace (#2)
    * feat: echo command trace
    
    verbosity to help with debugging
    
    * feat: make entire entrypoint verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -xe
 
 export KUBECONFIG="${PWD}/kubeconfig"
 eksctl utils write-kubeconfig --cluster $INPUT_EKS_CLUSTER --kubeconfig $KUBECONFIG
@@ -19,6 +19,6 @@ fi
 
 echo "running entrypoint command(s)"
 
-response=$(sh -c "$INPUT_COMMAND")
+response=$(sh -xc "$INPUT_COMMAND")
 
 echo "::set-output name=response::$response"
